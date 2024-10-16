852fa86033559e0a001c3182de99ecb9c27e841b
FernandoSoso <fernandososo324@gmail.com>
2024-10-16 15:35:33 -0300

    chore: Adição da biblioteca do Springdoc

--- src/main/java/br/com/boss/app/bossapi/BossApiApplication.java
+++ src/main/java/br/com/boss/app/bossapi/BossApiApplication.java
@@ -1,8 +1,19 @@
 package br.com.boss.app.bossapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "boss API",
+                version = "1.0",
+                description = "Documentação da API boss",
+                contact = @Contact(name = "Suporte", email = "suporte@exemplo.com")
+        )
+)
 @SpringBootApplication
 public class BossApiApplication {
 
