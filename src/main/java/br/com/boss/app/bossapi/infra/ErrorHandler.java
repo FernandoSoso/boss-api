cf5cbc4d6b73dd44e1f11dbfc8f6730e47b82082
Fernando Soso <fernandososo324@gmail.com>
2024-11-13 12:54:14 -0300

    refactor/feat: Refatoração do ErrorHandler

--- src/main/java/br/com/boss/app/bossapi/infra/ErrorHandler.java
+++ src/main/java/br/com/boss/app/bossapi/infra/ErrorHandler.java
@@ -1,16 +1,49 @@
 package br.com.boss.app.bossapi.infra;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 @RestControllerAdvice
+@Tag(name = "Erros", description = "Tratamento de erros")
 public class ErrorHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity error404Handler (){
+    public ResponseEntity<?> error404NotFoundHandler() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<?> error400BadRequestHandler(MethodArgumentTypeMismatchException ex) {
+        return ResponseEntity.badRequest().body("URL mal formatada: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> errorInvalidData(MethodArgumentNotValidException ex) {
+        List<FieldError> errors = ex.getFieldErrors();
+        List<DadosErroValidacao> dados = new ArrayList<>();
+        for (FieldError fe : errors) {
+            dados.add(new DadosErroValidacao(fe.getField(), fe.getDefaultMessage()));
+        }
+        return ResponseEntity.badRequest().body(dados);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<?> error400BadRequestHandler(BadRequestException ex) {
+        DadosErroValidacao dados = new DadosErroValidacao("Erro", ex.getMessage());
+
+        return ResponseEntity.badRequest().body(dados);
+    }
+    private record DadosErroValidacao(String campo, String mensagem) {
     }
 }
